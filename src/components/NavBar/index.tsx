915be2e5827714f536bb51bc99d46100ca382369
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-01 21:09:04 +0545

    add bottom border , make sticky

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -11,6 +11,9 @@ export default function NavBar() {
         <nav className="h-10 md:h-16 bg-bgColor1 flex items-center justify-between px-2
         md:px-10
         lg:px-16
+        border-b border-gray-800
+        sticky top-0 z-10
+        
         ">
             <span className="flex items-center">
                 <Logo />
