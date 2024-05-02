74b7ff19d39add66ffe03697e2501df278b7f017
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-02 17:21:02 +0545

    add movie selection to home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom";
-import MovieCards from "../MovieCards";
+import MovieSection from "./MovieSection";
 
 export default function Home() {
     return (
-        <section className="bg-bgColor2 flex flex-col items-center">
-            <span className="flex justify-between w-4/5 py-10">
-                <p className=" font-bold text-white  ">Latest YIFY Movies Torrents</p>
-                <Link to="/browse-movies" className="text-text2">Browse all</Link>
-            </span>
-            <MovieCards className="grid grid-cols-4 gap-x-16 gap-y-4" />
-        </section>
+        <>
+            <MovieSection title="Latest YIFY Movies Torrents" sort_by="date_added" bgColor="bg-bgColor2"/>
+            <MovieSection title="Top Rated" sort_by="rating" bgColor="bg-bgColor1"/>
+            <MovieSection title="Most Downloaded" sort_by="download_count" bgColor="bg-bgColor2"/>
+            <MovieSection title="Most Liked" sort_by="like_count" bgColor="bg-bgColor1"/>
+            
+        </>
     )
 }
+
+
+            // <section className="bg-bgColor2 flex flex-col items-center">
+            //     <span className="flex justify-between w-4/5 py-10">
+            //         <p className=" font-bold text-white  ">Latest YIFY Movies Torrents</p>
+            //         <Link to="/browse-movies" className="text-text2">Browse all</Link>
+            //     </span>
+            //     {/* latest movies */}
+            //     {/* <MovieCards className="grid grid-cols-4 gap-x-16 gap-y-4" /> */}
+            //     <LatestMovie />
+            // </section>
