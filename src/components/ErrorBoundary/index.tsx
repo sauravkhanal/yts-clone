4632e2f91cc5f51a027efa3b0bc003b484f7fb87
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-02 23:25:08 +0545

    add error boundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,47 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import Button from '../Button';
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+        // You can also log the error to an error reporting service
+        console.error('ErrorBoundary caught an error:', error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <>
+                <div className='flex flex-col gap-10 justify-center items-center h-screen bg-bgColor1'>
+                    <p className='font-bold text-2xl text-white'>
+                        Something went wrong
+                    </p>
+                    <span className='flex gap-5 my-5'>
+                        <Button>Go Back</Button>
+                        <Button>Home</Button>
+                        <Button>Report</Button>
+                    </span>
+                </div>
+            </>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ErrorBoundary from './components/ErrorBoundary/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
