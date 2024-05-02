08b3fa4192de3d1c77cbf8026720676e76bf555f
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-02 15:56:21 +0545

    implement query

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,20 @@
 import axios, { AxiosResponse } from "axios";
-import { Data, Root } from "../types";
+import { Data, MovieQuery, Root } from "../types";
 
 export const getAxios = axios.create({
     baseURL: "https://yts.mx/api/v2"
 });
 
-export const getMoviesData = async ():Promise<Data | null> => {
+export const getMoviesData = async (query?: MovieQuery):Promise<Data | undefined> => {
     try {
-        const response: AxiosResponse<Root> = await getAxios.get("/list_movies.json");
+        const response: AxiosResponse<Root> = await getAxios.get("/list_movies.json", {params: query});
+        console.log(query)
         console.log("The response is: ")
         console.log(response.data)
         return response.data.data;
         
     } catch (error) {
         console.error("1. Error fetching movies", error);
-        return null;
+        return ;
     }
 };
--- src/components/Browse/index.tsx
+++ src/components/Browse/index.tsx
@@ -1,5 +1,32 @@
-export default function  Browse() {
+import { useState, useEffect } from "react";
+import { getMoviesData } from "../../api";
+import { MovieQuery, Data } from "../../types";
+import MovieCards from "../MovieCards";
+import Search from "./Search";
+
+export default function Browse() {
+    const [data, setData] = useState<Data>();
+    const [query, setQuery] = useState<MovieQuery>({});
+
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getMoviesData(query);
+            console.log("The received data is ")
+            // console.log(data)
+            setData(response);
+        }
+        fetchData();
+    }, [query]);
     return (
-      <div className="flex justify-center items-center text-3xl w-full h-full bg-green-700">Browse</div>
+        <>
+            <Search setQuery={setQuery} />
+
+            <section className="bg-bgColor2 flex flex-col items-center">
+                <MovieCards className="grid grid-cols-4 gap-x-16 gap-y-4 bg-bgColor1" data={data?.movies}/>
+            </section>
+
+            {/* <button onClick={()=>setQuery({limit:5})}>click me</button> */}
+        </>
     )
-  }
+}
