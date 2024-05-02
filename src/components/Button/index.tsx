785e155c4305862edf949a9b1e197806eae27894
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-02 16:48:57 +0545

    add hover effect

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -12,7 +12,7 @@ export default function Button({ url, className, children }: IButton) {
     // }
 
     return <button
-        className={`rounded-[.18rem] bg-accent_green h-9 w-32 font-bold text-base text-white  ${className}`}
+        className={`rounded-[.18rem] bg-accent_green h-9 w-32 font-bold text-base text-white hover:opacity-80 transition  ${className}`}
     >
         <a href={url}>{children}</a>
     </button>
