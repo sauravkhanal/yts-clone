66a8f7fed087bee3728e286203d7df29f9663a3b
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-03 12:15:50 +0545

    add interface for detail page

--- src/types/index.ts
+++ src/types/index.ts
@@ -96,3 +96,46 @@ export interface PaginationDetails {
     limit: number;
     setQuery: React.Dispatch<React.SetStateAction<MovieQuery>>;
 }
+
+
+export interface Cast {
+    name: string
+    character_name: string
+    url_small_image?: string
+    imdb_code: string
+}
+
+export interface MovieDetail {
+    id: number
+    url: string
+    imdb_code: string
+    title: string
+    title_english: string
+    title_long: string
+    slug: string
+    year: number
+    rating: number
+    runtime: number
+    genres: string[]
+    like_count: number
+    description_intro: string
+    description_full: string
+    yt_trailer_code: string
+    language: string
+    mpa_rating: string
+    background_image: string
+    background_image_original: string
+    small_cover_image: string
+    medium_cover_image: string
+    large_cover_image: string
+    medium_screenshot_image1: string
+    medium_screenshot_image2: string
+    medium_screenshot_image3: string
+    large_screenshot_image1: string
+    large_screenshot_image2: string
+    large_screenshot_image3: string
+    cast: Cast[]
+    torrents: Torrent[]
+    date_uploaded: string
+    date_uploaded_unix: number
+}
