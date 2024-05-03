290b28a4f2278f6881ad85605cb1f91901a843dd
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-03 10:21:45 +0545

    add normal variant and onClick mechanism

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,41 +3,41 @@ import { IClassName } from "../../types";
 interface IButton extends IClassName {
     url?: string;
     children?: React.ReactNode;
-    variant?: "outline";
+    variant?: "outline" | "normal";
     // disabled?: "boolean";
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
     [key: string]: any;
 }
 
-export default function Button({ variant, url, className, children, ...rest }: IButton) {
+export default function Button({ variant, url, className, children, onClick, ...rest }: IButton) {
 
-    // const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>)=> {
-    //     event.preventDefault()
-    // }
 
     return <>
-        {/* <button
-            className={`rounded-[.18rem] bg-accent_green h-9 w-32 font-bold text-base text-white hover:opacity-80 transition  ${className}`}
-        >
-            <a href={url}>{children}</a>
-        </button> */}
+
         {
             variant === "outline" ?
                 <button
                     className={`rounded-[.18rem] bg-transparent font-bold text-base text-white hover:bg-accent_green transition border border-text2 px-3 py-1  
                     disabled:opacity-50 disabled:hover:bg-transparent
+                    active:scale-110
                     ${className} `}
                     {...rest}
+                    onClick={onClick}
                 >
-                    <a href={url}>{children}</a>
+                    {/* <a href={url}>{children}</a> */}
+                    {children}
                 </button>
                 :
                 <button
                     className={`rounded-[.18rem] bg-accent_green px-4 py-[6px] font-bold text-base text-white hover:opacity-80 transition  
                     disabled:opacity-50 disabled:hover:bg-transparent
+                    active:scale-110
                     ${className}`}
                     {...rest}
+                    onClick={onClick}
                 >
-                    <a href={url}>{children}</a>
+                    {/* <a href={url}>{children}</a> */}
+                    {children}
                 </button>
         }
     </>
