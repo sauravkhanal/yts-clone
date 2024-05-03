d4e7755d70af73c5582b2494c76293c773229e50
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 00:36:18 +0545

    navigate to home on icon press

--- src/components/NavBar/Logo.tsx
+++ src/components/NavBar/Logo.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom"
 import { IClassName } from "../../types"
 
 function Logo({ className }: IClassName) {
+	const navigator = useNavigate()
 	return (
 		<img src="https://yts.mx/assets/images/website/logo-YTS.svg" alt="yts logo"
-			className={`h-7 md:h-10 ${className}`} />
+			className={`h-7 md:h-10 cursor-pointer active:scale-90 transition ${className}`} onClick={()=>navigator("/")}/>
 	)
 }
 
