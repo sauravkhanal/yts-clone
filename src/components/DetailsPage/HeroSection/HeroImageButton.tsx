7fdf8721eb629c42669b6f5f1b28c1313358e873
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 15:26:31 +0545

    increase btn padding and image size

--- src/components/DetailsPage/HeroSection/HeroImageButton.tsx
+++ src/components/DetailsPage/HeroSection/HeroImageButton.tsx
@@ -11,13 +11,13 @@ export default function HeroImageButton(): JSX.Element {
 	const details = useContext(DetailsContext);
 
 	return (
-		<div className="flex flex-col w-64 gap-2">
+		<div className="flex flex-col w-72 gap-2">
 			<img
 				src={details?.large_cover_image}
 				alt={details?.title}
 				className="border-[5px] border-white rounded sm"
 			/>
-			<Button>
+			<Button className="py-2"> 
 				<a
 					href={details?.torrents[details?.torrents.length - 1]?.url} // link to last torrent url
 					className="flex justify-center items-center gap-2"
@@ -26,7 +26,7 @@ export default function HeroImageButton(): JSX.Element {
 					Download
 				</a>
 			</Button>
-			<Button className="bg-[#0aad9e]">Watch Now</Button>
+			<Button className="bg-[#0aad9e] py-2">Watch Now</Button>
 		</div>
 	);
 }
