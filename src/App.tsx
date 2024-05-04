7a6bbb1aab42b76d7e0d0230a5970c1027f872c1
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 15:44:34 +0545

    make page cover screen on minimum

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import Home from "./components/Home"
 import Browse from "./components/Browse"
 import Footer from "./components/Footer"
 import DetailsPage from "./components/DetailsPage"
+import PageNotFound from "./components/404"
 
 function App() {
 
 	return (
-		<div className="font-arimo">
+		<div className="font-arimo flex flex-col min-h-screen">
 			<NavBar />
 
 			<Routes>
 				<Route path="/" Component={Home} />
 				<Route path="/browse-movies" Component={Browse}/>
 				<Route path="/details/:id" Component={DetailsPage}/>
+				<Route path="*" Component={PageNotFound}/>
 			</Routes>
 
 			<Footer/>
