e6263292b7239e8812bbccd523ad16b66785cfbd
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 14:07:39 +0545

    add go to home

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component, ErrorInfo, ReactNode } from 'react';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 interface ErrorBoundaryProps {
     children: ReactNode;
@@ -24,20 +25,24 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
         console.error('ErrorBoundary caught an error:', error, errorInfo);
     }
 
+    goBack = () => {
+        window.history.back();
+    };
+
     render() {
         if (this.state.hasError) {
-            return <>
+            return (
                 <div className='flex flex-col gap-10 justify-center items-center h-screen bg-bgColor1'>
                     <p className='font-bold text-2xl text-white'>
                         Something went wrong
                     </p>
                     <span className='flex gap-5 my-5'>
-                        <Button>Go Back</Button>
-                        <Button>Home</Button>
-                        <Button>Report</Button>
+                        <Button disabled>Go Back</Button>
+                        <Button><a href="/">Home</a></Button>
+                        <Button disabled>Report</Button>
                     </span>
                 </div>
-            </>
+            );
         }
 
         return this.props.children;
