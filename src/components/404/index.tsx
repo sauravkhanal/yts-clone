4cbf3899e3165e039ff3d93f39fa955b31d84219
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 15:43:52 +0545

    add 404 page

---
+++ src/components/404/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Button from "../Button";
+
+export default function PageNotFound() {
+  return (
+    <div className="bg-bgColor2 flex flex-col justify-center items-center gap-5 text-white flex-grow">
+        <p className="text-6xl font-bold ">Oops!</p>
+        <p>404 - PAGE NOT FOUND</p>
+        <p className="text-center max-w-md">The page you are looking for might have been removed, had it's name changed or temporarily unavailable</p>
+        <Button><Link to="/">GO TO HOMEPAGE</Link></Button>
+    </div>
+  )
+}
