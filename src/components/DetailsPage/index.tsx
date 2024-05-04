2be7e5067fb24fe064c1f3dee58a3b6c921d422d
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 23:30:22 +0545

    handle invalid id,  add 5s wait for api response

--- src/components/DetailsPage/index.tsx
+++ src/components/DetailsPage/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { MovieDetail } from "../../types";
 import { getMovieDetail } from "../../api";
 import HeroSection from "./HeroSection";
@@ -11,53 +11,39 @@ export const DetailsContext = createContext<MovieDetail | undefined>(undefined);
 
 
 export default function DetailsPage() {
+	const navigate = useNavigate()
+	const navigateToErrorPage = () => navigate("/404")
 
 	const { id } = useParams<string>();
 	const [details, setDetails] = useState<MovieDetail | undefined>(undefined)
 	const [ytModalVisible, setYtModalVisible] = useState<boolean>(false)
-	const toggleModal = () => {
-		setYtModalVisible((v) => !v)
-	}
+
 
 	useEffect(() => {
+		const timer = setTimeout(navigateToErrorPage, 5000) // show error if data can't be fetched in 5s
 		async function fetchData() {
 			if (id) {
 				const response = await getMovieDetail(parseInt(id));
-				if (response) setDetails(response?.data.movie);
+				if (response) {
+					setDetails(response?.data.movie);
+				}
+
+				if (response?.data.movie.id === 0 || response?.data.movie.id == undefined) navigate('/404')
+				else if (!response) navigate('/404')
+
+				clearTimeout(timer)
 				// console.log(JSON.stringify(details))
 				// console.log(`The x response is : ${JSON.stringify(response?.data.movie)}`)
 			}
 		}
 		fetchData();
+
+		return ()=> clearTimeout(timer)
 	}, [id]);
 
 
-	// 	return (
-	// 		<div style={{
-	// 			backgroundImage: `url(${details?.background_image})`,
-	// 			backgroundSize: 'cover',
-	// 			backgroundPosition: 'center',
-	// 			backgroundRepeat: 'no-repeat'
-	// 		}}
-	// 			className="relative bg-bgColor2 flex flex-col min-h-screen  justify-center items-center"
-	// 		>
-	// 			<div className="absolute inset-0 bg-gradient-to-b from-transparent to-bgColor2 backdrop-filter backdrop-blur-lg">
 
-	// 			<div className="flex justify-evenly gap-20 py-10 md:px-20 px-32 relative max-w-screen-lg">
-	// 				<ImageBtnContainer large_cover_image={details?.large_cover_image} title={details?.title} />
-	// 				<TextContainer {...details} />
-	// 				<SimilarMovies id={id} />
-	// 			</div>
-	// 				<YoutubeModal video_id={details?.yt_trailer_code} isModalOpen={isModalOpen} toggleModal={toggleModal} />
-	// 			</div>
-	// 			<Screenshots onClick={toggleModal}
-	// 				ss1={details?.medium_screenshot_image1}
-	// 				ss2={details?.medium_screenshot_image2}
-	// 				ss3={details?.medium_screenshot_image3}
-	// 			/>
-	// 		</div>
-	// 	);
-	// }
+
 	return (
 		<DetailsContext.Provider value={details}>
 			<div>
