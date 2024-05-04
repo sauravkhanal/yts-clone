44520d5f5bc120da218901074b4f54fca5eec972
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 14:58:11 +0545

    implement screenshot section

--- src/components/DetailsPage/Screenshots.tsx
+++ src/components/DetailsPage/Screenshots.tsx
@@ -1,33 +1,25 @@
-import { IoPlayCircleOutline } from "react-icons/io5";
+import { useContext } from "react";
+import { PiPlayCircleThin } from "react-icons/pi";
+import { DetailsContext } from ".";
 
-interface IScreenshots {
-    ss1?: string,
-    ss2?: string,
-    ss3?: string,
-    onClick: any
-}
 
-export default function Screenshots({ ss1, ss2, ss3, onClick }: IScreenshots) {
+
+export default function Screenshots( {setVisible} : {setVisible: React.Dispatch<React.SetStateAction<boolean>>}) {
+    const details = useContext(DetailsContext)
     return (
-        <div className="flex flex-row gap-1 justify-center max-h-60 w-auto z-10 my-10">
-            <div className="relative " >
-                <img src={ss1} alt="screen shot1" className="hover:opacity-70 transition  object-contain" />
-                <div
-                    onClick={onClick}
-                    className="absolute top-0 w-full h-full flex flex-col justify-center items-center hover:scale-110 transition"
-                >
-                    <IoPlayCircleOutline className="text-6xl text-white" />
+        <div className={`flex flex-row gap-1 justify-center py-10 bg-bgColor2 `}>
+            <div
+                className="relative flex justify-center items-center group cursor-pointer"
+                onClick={()=>setVisible(true)}
+            >
+                <img src={details?.medium_screenshot_image1} alt="screen shot1" className="group-hover:opacity-70 transition  object-contain" />
+                <span className="absolute group-hover:scale-110 transition pt-10">
+                    <PiPlayCircleThin className="text-6xl text-white" />
                     <p className="font-semibold text-white text-xl">Trailer</p>
-                </div>
+                </span>
             </div>
-            <div>
-
-                <img src={ss2} alt="screen shot2" className="hover:opacity-70 transition object-contain " />
-            </div>
-            <div>
-
-                <img src={ss3} alt="screen shot3" className="hover:opacity-70 transition object-contain" />
-            </div>
+            <img src={details?.medium_screenshot_image2} alt="screen shot2" className="hover:opacity-70 transition object-contain " />
+            <img src={details?.medium_screenshot_image3} alt="screen shot3" className="hover:opacity-70 transition object-contain" />
         </div>
     )
 }
