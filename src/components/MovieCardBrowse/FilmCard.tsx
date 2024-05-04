2513f9ae458bec03d6821f811fb83a028e7761ed
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 15:17:31 +0545

    add link to detail on title

--- src/components/MovieCardBrowse/FilmCard.tsx
+++ src/components/MovieCardBrowse/FilmCard.tsx
@@ -1,34 +1,7 @@
 import { Movie } from "../../types";
 import HoverContent from "./HoverContent";
 
-export default function FilmCard({
-    id,
-    url,
-    imdb_code,
-    title,
-    title_english,
-    title_long,
-    slug,
-    year,
-    rating,
-    runtime,
-    genres,
-    summary,
-    description_full,
-    synopsis,
-    yt_trailer_code,
-    language,
-    mpa_rating,
-    background_image,
-    background_image_original,
-    small_cover_image,
-    medium_cover_image,
-    large_cover_image,
-    state,
-    torrents,
-    date_uploaded,
-    date_uploaded_unix,
-}: Movie) {
+export default function FilmCard(details: Movie) {
     return (
         <>
             <div>
@@ -42,17 +15,17 @@ export default function FilmCard({
                             group
                             relative
                             "
-                    id={id.toString()}
+                    id={details.id.toString()}
                 >
-                    {background_image ? <img src={medium_cover_image} className="min-w-full min-h-full object-cover" /> : ""}
-                    <HoverContent genres={genres} rating={rating} url={url} id={id} className="absolute top-0" />
+                    {details.background_image ? <img src={details.medium_cover_image} className="min-w-full min-h-full object-cover" /> : ""}
+                    <HoverContent genres={details.genres} rating={details.rating} id={details.id} className="absolute top-0" />
                 </div>
 
 
                 {/* text */}
                 <div>
-                    <p className="text-white font-bold hover:text-text2 max-w-[170px] line-clamp-1 overflow-clip"><a href="">{title ?? "Title"}</a></p>
-                    <p className="text-text2 text-[13.6px] ">{year ?? "Year"}</p>
+                    <p className="text-white font-bold hover:text-text2 max-w-[170px] line-clamp-1 overflow-clip"><a href={`/details/${id}`}>{details.title ?? "Title"}</a></p>
+                    <p className="text-text2 text-[13.6px] ">{details.year ?? "Year"}</p>
                 </div>
 
             </div>
