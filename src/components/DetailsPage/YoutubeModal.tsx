246440bbeaca4a398cdd2b0ac90de0356df4718c
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 14:57:50 +0545

    implement youtube trailer modal

--- src/components/DetailsPage/YoutubeModal.tsx
+++ src/components/DetailsPage/YoutubeModal.tsx
@@ -1,15 +1,29 @@
-export default function YoutubeModal({ video_id, isModalOpen, toggleModal }: { video_id: string | undefined, isModalOpen: boolean, toggleModal: React.Dispatch<React.SetStateAction<boolean>> }) {
+import { useContext } from "react";
+import { DetailsContext } from ".";
+
+export default function YoutubeModal({ isModalVisible, setModalVisible }: { isModalVisible: boolean, setModalVisible: React.Dispatch<React.SetStateAction<boolean>> }) {
+    const details = useContext(DetailsContext)
     return (
-        isModalOpen &&
-        <div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-90 flex justify-center items-center z-50">
-            <button className="absolute top-10 right-[300px] text-white text-2xl hover:opacity-80" onClick={toggleModal}>X</button>
-            <iframe
-                title="YouTube Video"
-                width="800"
-                height="450"
-                src={`https://www.youtube.com/embed/${video_id}`}
-                allowFullScreen
-            ></iframe>
+        isModalVisible &&
+        <div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-90 flex justify-center items-center z-10 " 
+        onClick={() => setModalVisible(false)} 
+        >
+            <div className="flex flex-col">
+
+                <button
+                    className=" text-text2 font-bold text-3xl hover:scale-125 transition self-end my-2"
+                    onClick={() => setModalVisible(false)}
+                >
+                    x
+                </button>
+                <iframe
+                    title="YouTube Video"
+                    width="800"
+                    height="450"
+                    src={`https://www.youtube.com/embed/${details?.yt_trailer_code}`}
+                    allowFullScreen
+                ></iframe>
+            </div>
         </div>
     );
 }
