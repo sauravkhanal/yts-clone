a7bff6edaf39d72800067726ed7e90e261470f99
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-04 15:52:10 +0545

    add nullish coalescing operator

--- src/components/Browse/index.tsx
+++ src/components/Browse/index.tsx
@@ -26,9 +26,9 @@ export default function Browse() {
 
             <section className="bg-bgColor1 flex flex-col gap-10 items-center py-5 flex-grow">
                 {data.movie_count > 0 && <p className="text-2xl text-accent_green">YIFY Movies - page {data.page_number}</p>}
-                <Pagination page_number={data.page_number} limit={data.limit} movie_count={data.movie_count} setQuery={setQuery} />
+                <Pagination page_number={data.page_number ?? 0} limit={data.limit ?? 0} movie_count={data.movie_count} setQuery={setQuery} />
                 <MovieCards className="grid grid-cols-5 gap-x-16 gap-y-4 bg-bgColor1 my-4" data={data?.movies} />
-                <Pagination page_number={data.page_number} limit={data.limit} movie_count={data.movie_count} setQuery={setQuery} />
+                <Pagination page_number={data.page_number ?? 0} limit={data.limit ?? 0} movie_count={data.movie_count} setQuery={setQuery} />
             </section>
 
             {/* <button onClick={()=>setQuery({limit:5})}>click me</button> */}
