6e85d92e00225102a32661601bee07c2471fbd76
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-05 15:23:13 +0545

    handle case with no name

--- src/components/DetailsPage/SummaryAndCast/Summary.tsx
+++ src/components/DetailsPage/SummaryAndCast/Summary.tsx
@@ -8,7 +8,7 @@ export default function Summary(className: IClassName) {
 		<div className={`px-4 ${className}`}>
 			<p className="text-xl text-text1 font-bold">Plot Summary</p>
 			<p className="text-text2 text-base py-6">{details?.description_full?.split("—")[0]}</p>
-			<p className="text-text2 text-base py-6"><span className="italic">Uploaded by: </span>{details?.description_full?.split("—")[1].toUpperCase()}</p>
+			<p className="text-text2 text-base py-6"><span className="italic">Uploaded by: </span>{details?.description_full?.split("—")[1]?.toUpperCase()}</p>
 		</div>
 	)
 }
