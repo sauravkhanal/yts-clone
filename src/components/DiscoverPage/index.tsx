b4651df54d73e2bd368d4ccd87812410f196b9cd
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-05 14:06:44 +0545

    add debounce on scroll handler

--- src/components/DiscoverPage/index.tsx
+++ src/components/DiscoverPage/index.tsx
@@ -5,38 +5,44 @@ import MovieCards from "../MovieCards";
 import { AxiosResponse } from "axios";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import LoadingSVG from "../NavBar/QuickSearch/LoadingSVG";
+import {debounceLeading } from "../../utils/debounce";
+import scrollToTop from "../../utils/ScrollToTop";
 
 
 export default function DiscoverPage(): JSX.Element {
     const [data, setData] = useState<Movie[]>([]);
-    const [page, setPage] = useState<number>(1);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(2)
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<{ message: string } | null>(null)
+    const limit = 20;
 
-    function increasePage(): void {
-        setPage(page => page + 1)
+    function increasePageCount(): void {
+        setCurrentPage((prevPage) => prevPage + 1)
     }
 
     /**
      * Fetches movie data from the API.
-     * @param {number} pageNumber - The page number to fetch.
+     * @param {number} currentPage - The page number to fetch.
      * @returns {Promise<void>} A Promise that resolves when the data is fetched.
      */
-    async function fetchData(pageNumber: number): Promise<void> {
+    async function fetchData(currentPage: number): Promise<void> {
         setLoading(true)
         setError(null)
         try {
 
             const params: MovieQuery = {
-                limit: 20,
-                page: pageNumber
+                limit,
+                page: currentPage,
+                // query_term: "the"
             };
-            const response: AxiosResponse<Root> = await getAxios.get("/list_movies.json", { params , timeout: 5000});
+            const response: AxiosResponse<Root> = await getAxios.get("/list_movies.json", { params, timeout: 5000 });
             setData(prev => ([...prev, ...response.data.data.movies]));
+            setTotalPages(Math.ceil(response.data.data.movie_count / limit))
 
         } catch (error: any) {
             console.log(error.message)
-            setError(error ?? {message:"An error occurred"})
+            setError(error ?? { message: "An error occurred" })
 
         } finally {
             setLoading(false)
@@ -45,36 +51,32 @@ export default function DiscoverPage(): JSX.Element {
 
 
     useEffect(() => {
-        !loading && fetchData(page);
-    }, [page]);
+        if (!loading && currentPage <= totalPages) {
+            fetchData(currentPage);
+        }
+    }, [currentPage]);
+
 
     function handleScroll() {
-        // console.log("***********************************************")
-        // console.log("Height of webpage: ", document.documentElement.scrollHeight)
-        // console.log("Scrolled from top: ", document.documentElement.scrollTop)
-        // console.log("total height of view port: ", window.innerHeight)
-
         const webpageHeight = document.documentElement.scrollHeight;
         const viewportHeight = window.innerHeight;
         const currentPosition = document.documentElement.scrollTop;
 
-        if (currentPosition >= webpageHeight - 2 * viewportHeight) { // fetch while there is still a viewport worth content is remaining
-            increasePage();
+        if ((currentPosition >= webpageHeight - viewportHeight) && (currentPage < totalPages) && (!loading)) {
+            increasePageCount();
         }
     }
 
+    // since scroll event is triggered very fast, the page number increases multiple times on scroll
+    // to prevent it the only leading call is executed (trailing calls are ignored) for 500ms
+    const debouncedScroll = debounceLeading(handleScroll, 500) 
+
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll)
-        return () => window.removeEventListener("scroll", handleScroll)
-    }, [])
+        window.addEventListener("scroll", debouncedScroll)
+        return () => window.removeEventListener("scroll", debouncedScroll)
+    }, [debouncedScroll])
 
 
-    function scrollToTop(): void {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
-    }
 
     return (
         <div className={`flex flex-col items-center py-10 bg-bgColor min-h-screen bg-bgColor1`}>
@@ -84,7 +86,10 @@ export default function DiscoverPage(): JSX.Element {
                     title="Move to top"
                     onClick={scrollToTop}
                 />
-                <p className="sticky top-20 flex justify-center p-2 text-sm text-text1">{data.length} movies</p>
+                <span className="text-text1 pt-1 font-bold text-sm">
+                    page {currentPage}/{totalPages}
+                </span>
+                <p className="sticky top-20 flex justify-center text-sm text-text1">{data.length} movies</p>
             </span>
             <p className=" font-bold text-text1 mb-10 text-xl ">Infinite Scrolling</p>
             <section className={`flex flex-col items-center bg-bgColor1`}>
@@ -92,6 +97,7 @@ export default function DiscoverPage(): JSX.Element {
             </section>
             {loading && <span className="m-5 w-8"><LoadingSVG /></span>}
             {error && <p className="m-5 text-2xl text-text1">{error.message}</p>}
+            {currentPage == totalPages && <p className="m-5 text-2xl text-text1">Reached the end</p>}
         </div>
 
     )
---
+++ src/utils/ScrollToTop.ts
@@ -0,0 +1,6 @@
+export default function scrollToTop(): void {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
